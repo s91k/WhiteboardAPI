b6fbee34164977f21dd0a44686c7db51327dac3c
Simon <s91k@hotmail.com>
2025-01-18 21:57:36 +0100

    Database update

--- data/entities.go
+++ data/entities.go
@@ -1,7 +1,7 @@
 package data
 
 type Pixel struct {
-	X     int    `gorm:"primaryKey;autoIncrement:false;size:6"`
-	Y     int    `gorm:"primaryKey;autoIncrement:false;size:6"`
-	Color string `gorm:"not null"`
+	X     int    `gorm:"primaryKey;autoIncrement:false;not null"`
+	Y     int    `gorm:"primaryKey;autoIncrement:false;not null"`
+	Color string `gorm:"not null;type:varchar(7)"`
 }
